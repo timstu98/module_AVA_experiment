3901cf5d3ea3aff84c39e08cc9a175efbc41b17d
Tim Stuart <timstu98@gmail.com>
2021-03-02 15:15:03 +0000

    My original commit, with the .gitignore used to avoid uploading the modules node files

---
+++ main.js
@@ -0,0 +1,4 @@
+import { adding, multiplying } from './myModule.js';
+
+console.log(adding(1,3));
+console.log(multiplying(2,8));
---
+++ myModule.js
@@ -0,0 +1,20 @@
+// export function adding(a,b) {
+//     return a+b;
+// }
+
+// export function multiplying(a,b) {
+//     return a*b;
+// }
+
+
+
+///////////////////////////////////////////////////////////
+
+function adding(a,b) {
+    return a+b;
+}
+function multiplying(a,b) {
+    return a*b;
+}
+
+export {adding,multiplying};
